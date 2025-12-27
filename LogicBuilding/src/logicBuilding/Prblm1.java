c2c13ff35011b08b39e5b50860689af58b4e1e92
MOH RAMJAN <ramjangori6666@gmail.com>
2025-12-27 22:19:33 +0530

    problem1-Extracting Digits from a number

--- LogicBuilding/src/logicBuilding/Prblm1.java
+++ LogicBuilding/src/logicBuilding/Prblm1.java
@@ -1,6 +1,21 @@
 package logicBuilding;
-
+import java.util.Scanner;   // import Scanner Class 
 //extracting digit from a number 
 public class Prblm1 {
-
+    public static void main(String[] arg) {
+    	Scanner sc = new Scanner(System.in);  // Creating Object of Scanner class 
+    	System.out.println("Enter Number :"); // Taking input from User 
+    	int n = sc.nextInt(); // Stroing number in a variable 
+    	
+    	
+    	// applying While loop to geting last digit of number until n>0
+    	while(n>0) {
+    		int digit = n%10;  // geting last digit from a number 
+    		
+    		System.out.println(digit); // Printing digit 
+    		n = n/10; // Updating Number 
+    	}
+    	 
+    	 
+    }
 }
